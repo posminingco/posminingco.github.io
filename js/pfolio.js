4dda6ca730d2a0c3453dd300eacd11f02d08b2bc
remche <remche@remche.org>
2018-05-25 12:55:18 +0200

    trying https for xsite content

--- js/pfolio.js
+++ js/pfolio.js
@@ -1,4 +1,4 @@
-$.getJSON('http://gsx2json.com/api?id=1kalWS1OK_WVXvg8RzOfllSlLdDR8VPruXwch9Mz4Zgc&sheet=1&q=USD&columns=false', function(data) {
+$.getJSON('https://gsx2json.com/api?id=1kalWS1OK_WVXvg8RzOfllSlLdDR8VPruXwch9Mz4Zgc&sheet=1&q=USD&columns=false', function(data) {
     var text = Math.floor(`${data.rows[0].btcvalue}`).toLocaleString();
     $(".pfvalue").html(text);
 });
