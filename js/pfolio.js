9ea7c4a59161977cabe8834166ffe89c90a46a61
remche <remche@remche.org>
2018-05-25 13:27:54 +0200

    using google sheet directly

--- js/pfolio.js
+++ js/pfolio.js
@@ -1,4 +1,4 @@
-$.getJSON('https://gsx2json.com/api?id=1kalWS1OK_WVXvg8RzOfllSlLdDR8VPruXwch9Mz4Zgc&sheet=1&q=USD&columns=false', function(data) {
-    var text = Math.floor(`${data.rows[0].btcvalue}`).toLocaleString();
+$.getJSON('https://spreadsheets.google.com/feeds/cells/1kalWS1OK_WVXvg8RzOfllSlLdDR8VPruXwch9Mz4Zgc/1/public/values?alt=json', function(data) {
+    var text = Math.floor(`${data.feed.entry[data.feed.entry.length -1 ].content.$t}`).toLocaleString();
     $(".pfvalue").html(text);
 });
